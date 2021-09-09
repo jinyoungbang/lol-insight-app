2222ca2fb76789b19a46498ea0718f668c62fec7
Taesung Yoon <yoontsy2000@gmail.com>
2021-09-09 16:20:16 +0900

    Home page GUI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,48 +1,84 @@
-
 import React, { useState } from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  BackgroundContainer,
-  TitleText,
-  CustomTextField,
-  CustomContainer
-} from "./helpers/Styles";
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Typography, Button, Paper } from '@material-ui/core'
+import { createStyles, createTheme, ThemeProvider } from "@material-ui/core/styles";
+import RegionDropdown from './components/RegionDropdown';
 
-import RegionDropdown from "./components/RegionDropdown";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5680E9'
+    },
+    secondary: {
+      light: '#84CEEB',
+      main: '#5AB9EA'
+    }
+  }
+})
+
+const useStyles = makeStyles((theme) => 
+  createStyles({
+    root: {
+      marginTop: '15%',
+    },
+    body: {
+      display: 'flex',
+      background: theme.palette.primary.main,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '50%',
+      margin: '0 auto',
+      padding: theme.spacing(2)
+    },
+    textfield: {
+      margin: theme.spacing(1),
+      width: '50%'
+    },
+    dropdown: {
+      margin: theme.spacing(1),
+      width: '100%'
+    },
+    button: {
+      margin: theme.spacing(1),
+      height: '100%'
+    }
+  }
+))
 
 const Home = () => {
-  const classes = useStyles();
   const [name, setName] = useState("");
+  const classes = useStyles();
   return (
-    <BackgroundContainer>
-      <CustomContainer>
-        <form className={classes.form} noValidate autoComplete="off">
-          <TitleText>whyphi</TitleText>
-          <CustomTextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="key"
-            label="Key"
-            id="name"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <div className={classes.heading}>
+          <Typography variant='h1' gutterBottom>lol-insight-api</Typography>
+        </div>
+        <Paper
+          className={classes.body}
+          elevation={3}
+        >
+          <TextField
+            className={classes.textfield}
+            label="Search Player Name"
+            id="mui-theme-provider-outlined-input"
           />
-          <RegionDropdown />
-        </form>
-      </CustomContainer>
-    </BackgroundContainer>
+          <RegionDropdown
+            className={classes.dropdown}
+          />
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            Search
+          </Button>
+        </Paper>
+      </div>
+    </ThemeProvider>
+    
   );
 };
-
-const useStyles = makeStyles((theme) => ({
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-}));
 
 export default Home;
--- src/components/Home/components/RegionDropdown.js
+++ src/components/Home/components/RegionDropdown.js
@@ -5,50 +5,12 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import InputBase from '@material-ui/core/InputBase';
 
-const BootstrapInput = withStyles((theme) => ({
-    root: {
-      'label + &': {
-        marginTop: theme.spacing(3),
-      },
-    },
-    input: {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }))(InputBase);
-
-  const useStyles = makeStyles((theme) => ({
-    margin: {
-      margin: theme.spacing(1),
-    },
-  }));
-  
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const RegionDropdown = () => {
   const classes = useStyles();
@@ -56,24 +18,21 @@ const RegionDropdown = () => {
   const handleChange = (event) => {
     setRegion(event.target.value);
   };
+
   return (
     <FormControl className={classes.margin}>
-        <InputLabel id="demo-customized-select-label">Age</InputLabel>
-        <Select
-          labelId="region-select-label"
-          id="region"
+      <InputLabel id="demo-simple-select-outlined-label">Region</InputLabel>
+      <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
           value={region}
           onChange={handleChange}
-          input={<BootstrapInput />}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+      >
+        <MenuItem value={'NA'}>NA</MenuItem>
+        <MenuItem value={'KR'}>KR</MenuItem>
+        <MenuItem value={'EU'}>EU</MenuItem>
+      </Select>
+    </FormControl>
   );
 };
 
